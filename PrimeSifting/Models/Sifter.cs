41f0a128175f22f9d0aa38925a94f9145d518e96
Will W <wjwat@onionslice.org>
2022-02-24 21:09:48 -0600

    Add two imps. of prime sieve

--- PrimeSifting/Models/Sifter.cs
+++ PrimeSifting/Models/Sifter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,12 +9,11 @@ namespace PrimeSifting.Models
     public static List<int> Sift(int input)
     {
       List<int> primes = Enumerable.Range(2, input - 1).ToList();
-      List<int> seq = Enumerable.Range(2, input - 1).ToList();
+      List<int> seq = new List<int>(primes);
+      int[] check = new int[] { 2, 3, 5, 7 };
 
       foreach (int item in seq)
       {
-        int[] check = new int[] { 2, 3, 5, 7 };
-
         for (int i = 0; i < check.Length; i++)
         {
           if (item % check[i] == 0 && item != check[i])
@@ -24,6 +24,24 @@ namespace PrimeSifting.Models
         }
       }
 
+      return primes;
+    }
+
+    public static List<int> Sift2(int input)
+    {
+      List<int> primes = Enumerable.Range(2, input).ToList();
+      int z = input;
+
+      for (int x = 2; x <= z; x++, z = primes.Last())
+      {
+        for (int i = x; i <= z; i++)
+        {
+          if (i % x == 0 && x != i)
+          {
+            primes.Remove(i);
+          }
+        }
+      }
       return primes;
     }
   }
