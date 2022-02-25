e4b09aa606402bfa11c6fdc9d56b0a98b515cd73
Will W <wjwat@onionslice.org>
2022-02-24 21:09:22 -0600

    Add UI to test timing of two imps. of prime sieve

--- PrimeSifting/Program.cs
+++ PrimeSifting/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using PrimeSifting.Models;
 
 namespace PrimeSifting
@@ -8,16 +9,25 @@ namespace PrimeSifting
   {
     public static void Main()
     {
-      Action<string> cwl = Console.WriteLine;
-      Action<string> cw = Console.Write;
-
-      cw("Enter your number: ");
-      int userNumber = int.Parse(Console.ReadLine());
-      List<int> primes = Sifter.Sift(userNumber);
-      cwl("New Sequence: ");
-      foreach (int i in primes)
+      for (int i = 0; i < 3; i++)
       {
-        cw($"{i} ");
+        int test = 100000;
+        var watch = new System.Diagnostics.Stopwatch();
+        var watch2 = new System.Diagnostics.Stopwatch();
+
+        watch.Start();
+        List<int> x = Sifter.Sift(test);
+        watch.Stop();
+
+        Console.WriteLine($"{x.Count}");
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        watch2.Start();
+        List<int> y = Sifter.Sift2(test);
+        watch2.Stop();
+
+        Console.WriteLine($"{y.Count}");
+        Console.WriteLine($"Execution Time: {watch2.ElapsedMilliseconds} ms");
       }
     }
   }
