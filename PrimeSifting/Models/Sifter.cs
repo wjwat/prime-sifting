5f64dc5c81c8d1cb9575a1ecc3cbf46660b4283a
Will W <wjwat@onionslice.org>
2022-02-27 14:08:12 -0600

    More exploration

--- PrimeSifting/Models/Sifter.cs
+++ PrimeSifting/Models/Sifter.cs
@@ -6,7 +6,7 @@ namespace PrimeSifting.Models
 {
   public class Sifter
   {
-    public static List<int> Sift(int input)
+    public static int Sift(int input)
     {
       List<int> primes = Enumerable.Range(2, input - 1).ToList();
       List<int> seq = new List<int>(primes);
@@ -24,12 +24,12 @@ namespace PrimeSifting.Models
         }
       }
 
-      return primes;
+      return primes.Count;
     }
 
-    public static List<int> Sift2(int input)
+    public static int Sift2(int input)
     {
-      List<int> primes = Enumerable.Range(2, input).ToList();
+      List<int> primes = Enumerable.Range(2, input-1).ToList();
       int z = input;
 
       for (int x = 2; x <= z; x++, z = primes.Last())
@@ -42,15 +42,15 @@ namespace PrimeSifting.Models
           }
         }
       }
-      return primes;
+      return primes.Count;
     }
 
-    public static List<int> Sift3(int input)
+    public static int Sift3(int input)
     {
       int z = input;
       List<int> primes = new List<int>() { 2 };
 
-      for (int x = 3; x < z; x++)
+      for (int x = 3; x <= z; x++)
       {
         if (x % 2 != 0)
         {
@@ -68,7 +68,129 @@ namespace PrimeSifting.Models
           }
         }
       }
-      return primes;
+
+      return primes.Count;
+    }
+
+    public static int Sift4a(int input)
+    {
+      int y = 3;
+      int z = input;
+      List<int> primes = new List<int>() { 2 };
+
+      while (y <= z)
+      {
+        primes.Add(y);
+        y += 2;
+      }
+
+      for (int x = 3; x <= z; x++, z = primes.Last())
+      {
+        for (int i = x; i <= z; i++)
+        {
+          if (i % x == 0 && x != i)
+          {
+            primes.Remove(i);
+          }
+        }
+      }
+
+      return primes.Count;
+    }
+
+    public static int Sift4b(int input)
+    {
+      int y = 3;
+      int z = input;
+      List<int> primes = new List<int>() { 2 };
+
+      while (y <= z)
+      {
+        primes.Add(y);
+        y += 2;
+      }
+
+      int x = 3;
+      while (x <= z)
+      {
+        int i = x;
+        while (i <= z)
+        {
+          if (i % x == 0 && i != x)
+          {
+            primes.Remove(i);
+          }
+          i++;
+        }
+        x++;
+        z = primes.Last();
+      }
+
+      return primes.Count;
+    }
+
+    public static int Sift5(int input)
+    {
+      bool[] primes = new bool[input+1];
+      Array.Fill(primes, true);
+
+      for (int x = 0; x <= input; x++)
+      {
+        if (x == 0 || x == 1)
+        {
+          primes[x] = false;
+          continue;
+        }
+
+        for (int i = x; i <= input; i++)
+        {
+          if (i % x == 0 && x != i)
+          {
+            primes[i] = false;
+          }
+        }
+      }
+
+      return Array.FindAll(primes, x => x).Length;
+    }
+
+    public static int Sift6(int input)
+    {
+      int primes = input;
+
+      for (int x = input; x >= 2; x--)
+      {
+        for (int i = 2; i <= primes; i++)
+        {
+          if (x % i == 0 && x != i)
+          {
+            primes -= 1;
+            break;
+          }
+        }
+      }
+      
+      // A prime of 1 does not count :)
+      return primes - 1;
+    }
+
+    public static int Sift7(int input)
+    {
+      List<int> primes = Enumerable.Range(2, input-1).ToList();
+      int z = input;
+
+      for (int x = input; x >= 2; x--, z = primes.Last())
+      {
+        for (int i = 2; i <= z; i++)
+        {
+          if (x % i == 0 && x != i)
+          {
+            primes.Remove(x);
+            break;
+          }
+        }
+      }
+      return primes.Count;
     }
   }
 }
--- PrimeSifting/Program.cs
+++ PrimeSifting/Program.cs
@@ -10,12 +10,17 @@ namespace PrimeSifting
   {
     public static void Main()
     {
-      const int ITERATIONS = 300;
-      const int MAXPRIME = 10000;
-      Func<int, List<int>>[] FUNCS = {
-        Sifter.Sift,
-        Sifter.Sift2,
-        Sifter.Sift3,
+      const int ITERATIONS = 1;
+      const int MAXPRIME = 100000;
+      Func<int, int>[] FUNCS = {
+        // Sifter.Sift,
+        // Sifter.Sift2,
+        // Sifter.Sift3,
+        // Sifter.Sift4a,
+        // Sifter.Sift4b,
+        Sifter.Sift5,
+        Sifter.Sift6,
+        Sifter.Sift7,
       };
 
       CallSifter(ITERATIONS, MAXPRIME, FUNCS);
@@ -24,12 +29,13 @@ namespace PrimeSifting
     public static void CallSifter(
       int iterations,
       int maxPrime,
-      Func<int, List<int>>[] f
+      Func<int, int>[] f
     )
     {
+      Console.WriteLine("Count of Primes up to: {0}", maxPrime);
       foreach(var siftCall in f)
       {
-        List<int> primeCount = new List<int>();
+        int primeCount = 0;
         List<long> callCountAverage = new List<long>();
         int callCount = 0;
 
@@ -40,17 +46,17 @@ namespace PrimeSifting
           var watch = new System.Diagnostics.Stopwatch();
           callCount += 1;
 
-          Console.Write($"\tElapsed Time[{callCount}]: ");
+          // Console.Write($"\tElapsed Time[{callCount}]: ");
 
           watch.Start();
           primeCount = siftCall(maxPrime);
           watch.Stop();
 
-          Console.WriteLine($"{watch.ElapsedMilliseconds} ms");
+          // Console.WriteLine($"{watch.ElapsedMilliseconds} ms");
           callCountAverage.Add(watch.ElapsedMilliseconds);
         }
         Console.WriteLine($"\tAverage Time: {callCountAverage.Sum() / callCount} ms");
-        Console.WriteLine($"\tPrime Count: {primeCount.Count}");
+        Console.WriteLine($"\tPrime Count: {primeCount}");
       }
     }
   }
